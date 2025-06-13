05aaef04fc397f8958748cb6dc899f05a013a6b5
--global <vbarman116@gmail.com>
2025-06-13 23:12:04 +0530

    Fixed Swagger dependency

--- internal/handler/healthz.go
+++ internal/handler/healthz.go
@@ -7,12 +7,12 @@ import (
 )
 
 // Healthz godoc
-// @Summary      Health check endpoint
-// @Description  Responds with a simple message to indicate the service is running
-// @Tags         health
-// @Produce      json
-// @Success      200  {object}  map[string]string  "pong response"
-// @Router       /healthz [get]
+//	@Summary		Health check endpoint
+//	@Description	Responds with a simple message to indicate the service is running
+//	@Tags			health
+//	@Produce		json
+//	@Success		200	{object}	map[string]string	"pong response"
+//	@Router			/healthz [get]
 func Healthz(router *gin.Context) {
 	router.JSON(http.StatusOK, gin.H{"message": "PONG"})
 }
