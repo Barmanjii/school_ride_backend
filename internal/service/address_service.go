450cae8cac592e8d7eeafeb962259633bac36673
--global <vbarman116@gmail.com>
2025-06-21 03:08:15 +0530

    Added: Address skeleton

---
+++ internal/service/address_service.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"context"
+
+	"school_ride_backend/internal/model"
+)
+
+func CreateAddress(ctx context.Context, address *model.Address) {
+}
+
+func GetAddress(ctx context.Context, id string) (*model.Address, error) {
+	return nil, nil
+}
+func UpdateAddress(ctx context.Context, address *model.Address) error {
+	return nil
+}
+func DeleteAddress(ctx context.Context, id string) error {
+	return nil
+}
+func ListAddresses(ctx context.Context) ([]*model.Address, error) {
+	return nil, nil
+}
