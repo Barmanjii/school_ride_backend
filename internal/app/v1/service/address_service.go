f76f8553a7747538d7c6e1d99e70a8f30c2bf72a
--global <vbarman116@gmail.com>
2025-06-23 02:42:14 +0530

    Created the Address Post call and it is working with the supabase db

--- internal/app/v1/service/address_service.go
+++ internal/app/v1/service/address_service.go
@@ -2,13 +2,24 @@ package service
 
 import (
 	"context"
+	"errors"
+	"school_ride_backend/internal/app/v1/config"
 	"school_ride_backend/internal/app/v1/model"
 )
 
-func CreateAddress(ctx context.Context, address *model.Address) (*model.Address, error) {
-	return address, nil
+func CreateAddress(ctx context.Context, address *model.Address) error {
+	query := `
+		INSERT INTO addresses (flat, street, landmark, city, state, pincode, country)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)`
+	cmdTag, err := config.DB.Exec(ctx, query, address.Flat, address.Street, address.Landmark, address.City, address.State, address.Pincode, address.Country)
+	if err != nil {
+		return err
+	}
+	if cmdTag.RowsAffected() != 1 {
+		return errors.New("no rows created for address")
+	}
+	return nil
 }
-
 func GetAddress(ctx context.Context, id string) (*model.Address, error) {
 	return nil, nil
 }
