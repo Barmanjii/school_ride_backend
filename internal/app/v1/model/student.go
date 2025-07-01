4b30e7d9d5c7fbfecd6d943f84cac79937ba7296
--global <vbarman116@gmail.com>
2025-07-02 01:37:58 +0530

    feat: Fixed student post call

--- internal/app/v1/model/student.go
+++ internal/app/v1/model/student.go
@@ -5,8 +5,8 @@ import "github.com/google/uuid"
 type Student struct {
 	Name     string    `json:"name"`
 	Age      int       `json:"age"`
-	Gender   uuid.UUID `json:"gender"`
-	Class    uuid.UUID `json:"class"`
+	Gender   string    `json:"gender"`
+	Class    string    `json:"class"`
 	SchoolID uuid.UUID `json:"school_id"`
 }
 
@@ -14,7 +14,7 @@ type StudentInDatabase struct {
 	ID       uuid.UUID `json:"id"`
 	Name     string    `json:"name"`
 	Age      int       `json:"age"`
-	Gender   uuid.UUID `json:"gender"`
-	Class    uuid.UUID `json:"class"`
+	Gender   string    `json:"gender"`
+	Class    string    `json:"class"`
 	SchoolID uuid.UUID `json:"school_id"`
 }
