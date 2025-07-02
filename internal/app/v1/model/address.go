ab716d6f9f343c1d70e8e0f383dd1c75d0f7a8de
--global <vbarman116@gmail.com>
2025-07-03 02:32:59 +0530

    feat: Updated the model datatype int -> uuid

--- internal/app/v1/model/address.go
+++ internal/app/v1/model/address.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/google/uuid"
+
 type Address struct {
 	PlaceID     string `json:"place_id"`
 	FullAddress string `json:"full_address"`
@@ -8,9 +10,9 @@ type Address struct {
 }
 
 type AddressInDatabase struct {
-	ID          string `json:"id"`
-	PlaceID     string `json:"place_id"`
-	FullAddress string `json:"full_address"`
-	Latitude    string `json:"lat"`
-	Longitude   string `json:"lng"`
+	ID          uuid.UUID `json:"id"`
+	PlaceID     string    `json:"place_id"`
+	FullAddress string    `json:"full_address"`
+	Latitude    string    `json:"lat"`
+	Longitude   string    `json:"lng"`
 }
